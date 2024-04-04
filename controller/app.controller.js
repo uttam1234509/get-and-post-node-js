844845a967cb8a6d10fe07aa119c5b41430e2875
uttam1234509 <uttamradadiya.dreamspot@gmail.com>
2024-04-04 16:36:54 +0530

    gat and post

--- controller/app.controller.js
+++ controller/app.controller.js
@@ -1,16 +1,16 @@
 // const app = require('../model/app.model')
 
-// exports.findAll = (req, res) => {
-//     app.find()
-//         .then((data) => {
-//             res.send(data);
-//         }).catch((err) => {
-//             res.status(500).send({
-//                 user:
-//                     err.user || "occured error"
-//             })
-//         })
-// }
+exports.findAll = (req, res) => {
+    app.find()
+        .then((data) => {
+            res.send(data);
+        }).catch((err) => {
+            res.status(500).send({
+                user:
+                    err.user || "occured error"
+            })
+        })
+}
 
 const app = require("../model/app.model");
 
